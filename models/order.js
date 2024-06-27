7decf5d33fdb468d5c71b2958193b0d688840f69
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2024-06-27 15:26:56 +0500

    first commit

---
+++ models/order.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  user: {
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  products: [
+    {
+      product: {
+        type: Object,
+        // ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+    },
+  ],
+});
+
+orderSchema.methods.addOrder = function () {
+  return this.getCart()
+    .then((products) => {
+      const order = {
+        items: products,
+        user: {
+          _id: new ObjectId(this._id),
+          name: this.name,
+        },
+      };
+      return db.collection("orders").insertOne(order);
+    })
+    .then((result) => {
+      this.cart = { items: [] };
+      return db
+        .collection("users")
+        .updateOne(
+          { _id: new ObjectId(this._id) },
+          { $set: { cart: { items: [] } } }
+        );
+    });
+};
+
+module.exports = mongoose.model("Order", orderSchema);
